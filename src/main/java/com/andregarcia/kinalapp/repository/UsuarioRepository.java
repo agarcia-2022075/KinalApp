a658c5226cdcba468acf0305099e07b55af2d60c
agarcia-2022075 <agarcia-2022075@kinal.edu.gt>
2026-05-01 01:33:07 -0600

    feat: añadir método findByUsername al repositorio de usuarios

--- src/main/java/com/andregarcia/kinalapp/repository/UsuarioRepository.java
+++ src/main/java/com/andregarcia/kinalapp/repository/UsuarioRepository.java
@@ -3,8 +3,11 @@ package com.andregarcia.kinalapp.repository;
 import com.andregarcia.kinalapp.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     // buscar solo los usuarios activos
     List<Usuario> findByEstado(int estado);
+    
+    Optional<Usuario> findByUsername(String username);
 }
